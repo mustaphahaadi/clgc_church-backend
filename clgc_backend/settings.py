945ac59f024c6b57789484407650b756c0c2acab
mustaphahaadi <mustaphahaadi04@example.com>
2025-03-29 22:16:19 +0000

    chore: set allowed_all_origins to true

--- clgc_backend/settings.py
+++ clgc_backend/settings.py
@@ -63,10 +63,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:5173",
 # ]
