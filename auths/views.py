14c8adeffe1c686e2153085e6ecea5c403b5e853
blackbox24 <nicardid@gmail.com>
2025-04-02 18:27:58 +0000

    chore: removed profile endpoint

--- auths/views.py
+++ auths/views.py
@@ -9,9 +9,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from drf_yasg.utils import swagger_auto_schema
 
 from user.models import CustomUser
-from members.profile_model import Profile
 from .serializers import RegisterSerializer, LoginSerializer
-from .profile_serializer import ProfileSerializer
 
 
 # Register View
@@ -44,70 +42,3 @@ class LoginView(APIView):
             return Response(data,status=status.HTTP_202_ACCEPTED)
 
         return Response(serializer.errors,status=status.HTTP_401_BAD_REQUEST)
-
-
-class ProfileView(APIView):
-    permission_classes = (IsAuthenticated,)
-    serializers = ProfileSerializer
-    
-    def get_profile(self, user):
-        try:
-            # profile = get_object_or_404(Profile,user)
-            return Profile.objects.get(user=user)
-        except Profile.DoesNotExist:
-            return None
-    
-    @swagger_auto_schema(responses={200: ProfileSerializer})
-    def get(self, request,*args, **kwargs):
-        profile = self.get_profile(request.user)
-        if not profile:
-            return Response({"error": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = self.serializers(profile)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    @swagger_auto_schema(request_body=ProfileSerializer, responses={200: ProfileSerializer})
-    def put(self, request, *args, **kwargs):
-        profile = self.get_profile(request.user)
-        if not profile:
-            # Create a new profile if it doesn't exist
-            profile = Profile.objects.create(user=request.user)
-        
-        serializer = self.serializers(profile, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    @swagger_auto_schema(request_body=ProfileSerializer, responses={200: ProfileSerializer})
-    def patch(self, request, *args, **kwargs):
-        profile = self.get_profile(request.user)
-        if not profile:
-            # Create a new profile if it doesn't exist
-            profile = Profile.objects.create(user=request.user)
-        
-        serializer = self.serializers(profile, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @swagger_auto_schema(request_body=ProfileSerializer, responses={201: ProfileSerializer})
-    def post(self, request, *args, **kwargs):
-        # Check if profile already exists
-        profile = self.get_profile(request.user)
-        if profile:
-            return Response(
-                {"error": "Profile already exists. Use PUT or PATCH to update."}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        
-        # Create a new profile
-        serializer = self.serializers(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
