f2b7b3666e6020abf50b30e8adc7c01323619359
blackbox24 <nicardid@gmail.com>
2025-04-02 18:32:56 +0000

    feat: added custom permission of isAdmin or Readonly

--- user/permissions.py
+++ user/permissions.py
@@ -2,6 +2,6 @@ from rest_framework.permissions import BasePermission, AllowAny, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request == SAFE_METHODS:
+        if SAFE_METHODS in request.method:
             return True
         return request.user.is_superuser
