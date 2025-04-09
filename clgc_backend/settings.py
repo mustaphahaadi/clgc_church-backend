3d98bafff627921bda42037454a20aeca9481c87
blackbox24 <nicardid@gmail.com>
2025-04-09 12:32:37 +0000

    update: modified settings conf, remove default value for email port

--- clgc_backend/settings.py
+++ clgc_backend/settings.py
@@ -221,7 +221,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Email settings
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
