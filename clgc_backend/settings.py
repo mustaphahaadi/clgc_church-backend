369487d5a8b885356a1fac84d8910fc11ee1e1e8
blackbox24 <nicardid@gmail.com>
2025-04-16 18:29:13 +0000

    updated project conf access token expire in 6 mins

--- clgc_backend/settings.py
+++ clgc_backend/settings.py
@@ -118,7 +118,7 @@ SWAGGER_SETTINGS = {
 
 # SIMPLE JWT SETTINGS
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=6),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
